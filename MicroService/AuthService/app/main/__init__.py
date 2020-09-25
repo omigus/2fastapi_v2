c708a3278c5fef50c15a60795432dcdf9e6cf9be
Panudet Panumas <panudet.panumas@gmail.com>
2020-09-25 22:45:16 +0700

    Flask Limit

--- MicroService/AuthService/app/main/__init__.py
+++ MicroService/AuthService/app/main/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from .config import config_by_name
-
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 # db = SQLAlchemy()
 # ma = Marshmallow()
 flask_bcrypt = Bcrypt()
@@ -10,6 +11,11 @@ flask_bcrypt = Bcrypt()
 
 def create_app(config_name):
     app = Flask(__name__)
+    limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=[ "100 per minute"]
+    )
     cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
     app.config.from_object(config_by_name[config_name])
     # db.init_app(app)
